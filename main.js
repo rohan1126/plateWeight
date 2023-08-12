3d2b61ef387615b7cdde9f05382ccfd6109e6562
rohan1126 <rohanagni26@gmail.com>
2023-08-12 12:27:10 -0700

    added options

--- main.js
+++ main.js
@@ -1,60 +1,92 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const calculationRadioButtons = document.getElementsByName("calculation");
+  const weightInput = document.getElementById("weightInput");
+  const calculateButton = document.getElementById("calculateButton");
+  const output = document.getElementById("output");
+  const checkboxesContainer = document.getElementById("checkboxesContainer"); // New line
 
-document.addEventListener("DOMContentLoaded", function() {
-    const calculationRadioButtons = document.getElementsByName("calculation");
-    const weightInput = document.getElementById("weightInput");
-    const calculateButton = document.getElementById("calculateButton");
-    const output = document.getElementById("output");
-    const image = document.getElementById("images");
-
-    calculateButton.addEventListener("click", function() {
-        const selectedCalculation = document.querySelector("input[name='calculation']:checked").value;
-        const enteredWeight = parseFloat(weightInput.value);
-
-        if (isNaN(enteredWeight)) {
-            output.innerHTML = "Please enter a valid weight.";
-            return;
-        }
-
-        if (selectedCalculation === "requiredPlates") {
-            calculateRequiredPlates(enteredWeight);
-        } else if (selectedCalculation === "totalWeight") {
-            calculateTotalWeight(enteredWeight);
-        }
+  calculationRadioButtons.forEach((radio) => {
+    radio.addEventListener("change", function () {
+      checkboxesContainer.style.display =
+        radio.value === "requiredPlates" ? "block" : "none";
     });
+  });
+
+  calculateButton.addEventListener("click", function () {
+    const selectedCalculation = document.querySelector(
+      "input[name='calculation']:checked"
+    ).value;
+    const enteredWeight = parseFloat(weightInput.value);
+
+    if (isNaN(enteredWeight)) {
+      output.innerHTML = "Please enter a valid weight.";
+      return;
+    }
+
+    const selectedPlateWeights = getSelectedPlateWeights();
+
+    if (selectedCalculation === "requiredPlates") {
+      calculateRequiredPlates(enteredWeight, selectedPlateWeights);
+    } else if (selectedCalculation === "totalWeight") {
+      calculateTotalWeight(enteredWeight, selectedPlateWeights);
+    }
+  });
 });
 
-function calculateRequiredPlates(desiredWeight) {
-    const barWeight = 45;
-    const plateWeights = [45, 25, 10, 5, 2.5];
-    let remainingWeight = (desiredWeight - barWeight) / 2;
-    let plateCounts = {};
+function getSelectedPlateWeights() {
+  const selectedPlateWeights = [];
+  const plateWeightCheckboxes = document.querySelectorAll(
+    "input[name='plateWeight']:checked"
+  );
 
-    for (const plate of plateWeights) {
-        const count = Math.floor(remainingWeight / plate);
-        if (count > 0) {
-            plateCounts[plate] = count;
-            remainingWeight -= count * plate;
-        }
-        if (remainingWeight === 0) {
-            break;
-        }
-    }
+  plateWeightCheckboxes.forEach((checkbox) => {
+    selectedPlateWeights.push(parseFloat(checkbox.value));
+  });
 
-    if (remainingWeight === 0) {
-        let outputText = "Plate breakdown:<br>";
-        for (const plate in plateCounts) {
-            outputText += `${plateCounts[plate]} x ${plate} lb plates<br>`;
-        }
-        document.getElementById("output").innerHTML = outputText;
-        document.getElementById("images").innerHTML = outputText;
-    } else {
-        document.getElementById("output").innerHTML = "It's not possible to achieve the desired weight with the given plates.";
-    }
+  return selectedPlateWeights;
 }
 
+function calculateRequiredPlates(desiredWeight, selectedPlateWeights) {
+  const barWeight = 45;
+  let remainingWeight = (desiredWeight - barWeight) / 2;
+  let plateCounts = {};
+
+  for (const plate of selectedPlateWeights) {
+    const count = Math.floor(remainingWeight / plate);
+    if (count > 0) {
+      plateCounts[plate] = count;
+      remainingWeight -= count * plate;
+    }
+    if (remainingWeight === 0) {
+      break;
+    }
+  }
+
+  if (remainingWeight === 0) {
+    let outputText = "Plate breakdown:<br>";
+    let imageHtml = "";
+
+    for (const plate in plateCounts) {
+      outputText += `${plateCounts[plate]} x ${plate} lb plates<br>`;
+      imageHtml +=
+        `<img src="plate_${plate}.jpg" alt="${plate} lb Plate">`.repeat(
+          plateCounts[plate]
+        );
+    }
+
+    document.getElementById("output").innerHTML = outputText;
+    document.getElementById("images").innerHTML = imageHtml;
+  } else {
+    document.getElementById("output").innerHTML =
+      "It's not possible to achieve the desired weight with the given plates.";
+    document.getElementById("images").innerHTML = ""; // Clear images if not possible
+  }
+}
 
 function calculateTotalWeight(oneSideWeight) {
-    const barWeight = 45;
-    const totalWeight = (oneSideWeight * 2) + barWeight;
-    document.getElementById("output").innerHTML = `Total weight: ${totalWeight} lbs`;
+  const barWeight = 45;
+  const totalWeight = oneSideWeight * 2 + barWeight;
+  document.getElementById(
+    "output"
+  ).innerHTML = `Total weight: ${totalWeight} lbs`;
 }
